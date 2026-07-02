3b00118a487a31d5efcc784db5966953f08d91d0
Hamed Tanha <htanha@hotmail.com>
2026-07-02 21:23:02 +0200

    feat: add application navigation host

---
+++ app/src/main/java/de/hamedtanha/servertoolkit/navigation/AppNavHost.kt
@@ -0,0 +1,9 @@
+package de.hamedtanha.servertoolkit.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavHost() {
+    rememberNavController()
+}
