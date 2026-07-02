04cda7f9c817cd11db3366db1073cb8978a9e429
Hamed Tanha <htanha@hotmail.com>
2026-07-02 12:45:58 +0200

    Change

--- app/src/main/java/de/hamedtanha/servertoolkit/MainActivity.kt
+++ app/src/main/java/de/hamedtanha/servertoolkit/MainActivity.kt
@@ -1,47 +1,20 @@
-package de.hamedtanha.servertoolkit
+package de.hamedtanha.servertoolkit.ui.screens
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import de.hamedtanha.servertoolkit.ui.screens.HomeScreen
 import de.hamedtanha.servertoolkit.ui.theme.ServerToolkitTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             ServerToolkitTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ServerToolkitTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/de/hamedtanha/servertoolkit/ui/screens/ui/screens/HomeScreen.kt
@@ -0,0 +1,9 @@
+package de.hamedtanha.servertoolkit.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text("Server Toolkit")
+}
