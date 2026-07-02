abcc3a1fd8edf8afa64e83448315f85015b9c59d
Hamed Tanha <htanha@hotmail.com>
2026-07-02 14:33:11 +0200

    feat(domain): add initial server model

--- app/src/main/java/de/hamedtanha/servertoolkit/model/Server.kt
+++ app/src/main/java/de/hamedtanha/servertoolkit/model/Server.kt
@@ -1,17 +1,11 @@
 package de.hamedtanha.servertoolkit.model
-
 data class Server(
-    val id: String,
+    val id: Long = 0,
     val name: String,
     val host: String,
     val sshPort: Int = 22,
     val username: String,
     val description: String = "",
-    val favorite: Boolean = false,
-    val enabled: Boolean = true
+    val isFavorite: Boolean = false,
+    val isEnabled: Boolean = true
 )
-
-enum class AuthenticationMethod {
-    PASSWORD,
-    PRIVATE_KEY
-}
