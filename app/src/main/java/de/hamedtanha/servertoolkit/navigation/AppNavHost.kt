c1eb5b809eeff0d3c9519401d60471851746771b
Hamed Tanha <htanha@hotmail.com>
2026-07-02 21:34:03 +0200

    feat: configure application navigation graph

--- app/src/main/java/de/hamedtanha/servertoolkit/navigation/AppNavHost.kt
+++ app/src/main/java/de/hamedtanha/servertoolkit/navigation/AppNavHost.kt
@@ -1,9 +1,20 @@
 package de.hamedtanha.servertoolkit.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun AppNavHost() {
-    rememberNavController()
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = DashboardDestination.route,
+    ) {
+        composable(route = DashboardDestination.route) {
+            // Dashboard screen will be added in the next step.
+        }
+    }
 }
