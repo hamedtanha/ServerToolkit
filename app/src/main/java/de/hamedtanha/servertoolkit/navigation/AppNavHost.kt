cd9381c7005375bb893e8aae9b82a248079282ab
Hamed Tanha <htanha@hotmail.com>
2026-07-02 22:15:11 +0200

    feat: connect dashboard screen to navigation graph

--- app/src/main/java/de/hamedtanha/servertoolkit/navigation/AppNavHost.kt
+++ app/src/main/java/de/hamedtanha/servertoolkit/navigation/AppNavHost.kt
@@ -1,5 +1,5 @@
 package de.hamedtanha.servertoolkit.navigation
-
+import de.hamedtanha.servertoolkit.feature.dashboard.presentation.screen.DashboardScreen
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -14,7 +14,8 @@ fun AppNavHost() {
         startDestination = DashboardDestination.route,
     ) {
         composable(route = DashboardDestination.route) {
-            // Dashboard screen will be added in the next step.
+            DashboardScreen()
+
         }
     }
 }
