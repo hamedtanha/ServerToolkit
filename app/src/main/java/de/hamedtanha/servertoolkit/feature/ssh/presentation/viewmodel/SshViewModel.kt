0fcb067e0c61d3be98d3b926ad486c6158fcbc2d
Hamed Tanha <30567564+hamedtanha@users.noreply.github.com>
2026-07-04 14:26:34 +0200

    feat: add SSH placeholder view model

---
+++ app/src/main/java/de/hamedtanha/servertoolkit/feature/ssh/presentation/viewmodel/SshViewModel.kt
@@ -0,0 +1,24 @@
+package de.hamedtanha.servertoolkit.feature.ssh.presentation.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import de.hamedtanha.servertoolkit.feature.ssh.presentation.state.SshUiState
+import de.hamedtanha.servertoolkit.navigation.SshDestination
+import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+@HiltViewModel
+class SshViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+) : ViewModel() {
+
+    private val serverId: String = checkNotNull(savedStateHandle[SshDestination.SERVER_ID_ARGUMENT])
+
+    private val _uiState = MutableStateFlow(
+        SshUiState(serverId = serverId),
+    )
+    val uiState: StateFlow<SshUiState> = _uiState.asStateFlow()
+}
