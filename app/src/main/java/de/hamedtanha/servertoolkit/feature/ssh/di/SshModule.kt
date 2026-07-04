5c5ff1da3b119b3c66466f1e4d3214b22452ed76
Hamed Tanha <30567564+hamedtanha@users.noreply.github.com>
2026-07-04 18:48:48 +0200

    feat: bind ssh connection service

---
+++ app/src/main/java/de/hamedtanha/servertoolkit/feature/ssh/di/SshModule.kt
@@ -0,0 +1,20 @@
+package de.hamedtanha.servertoolkit.feature.ssh.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import de.hamedtanha.servertoolkit.feature.ssh.data.service.SshjConnectionService
+import de.hamedtanha.servertoolkit.feature.ssh.domain.service.SshConnectionService
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class SshModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindSshConnectionService(
+        service: SshjConnectionService,
+    ): SshConnectionService
+}
